09f851729c6242d018ca4b7b5e9e3d3b42f7c37b
Guto Xavier <gutoffline@gmail.com>
2015-11-07 19:17:40 -0200

    comit da versão final do jogo

--- main.js
+++ main.js
@@ -1,107 +1,126 @@
-// Initialize Phaser, and create a 400x490px game
+// Inicializa o Phaser, e cria uma áre de 400x490px para o jogo
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
-// Create our 'main' state that will contain the game
+// Cria estado PRINCIPAL do jogo com o conteúdo do jogo
 var mainState = {
 
     preload: function() { 
-        // This function will be executed at the beginning     
-        // That's where we load the game's assets  
+        // Esta função é executada no início
+        // Aqui são carregados os recursos(imagens, fundo, som,) para o jogo
 
-        // Change the background color of the game
+        // Altera a cor de fundo do jogo(da div gameDiv)
         game.stage.backgroundColor = '#71c5cf';
 
-        // Load the bird sprite
-        game.load.image('bird', 'assets/bird.png'); 
-
-        game.load.image('pipe', 'assets/pipe.png'); 
-
+        // Carrega o sprite do personagem
+        game.load.image('personagem', 'assets/personagem-bob.png'); 
+        // Carrega o sprite do bloco
+        game.load.image('bloco', 'assets/bloco-caveira.png'); 
+        // Carrega o som utilizado para o pulo
         game.load.audio('jump', 'assets/jump.wav');  
     },
 
     create: function() { 
-        // This function is called after the preload function     
-        // Here we set up the game, display sprites, etc.  
+        // esta função é chamada após a execução da função PRELOAD    
+        // Aqui é feita a configuração do jogo(personagem, blocos, som, etc)
 
-        // Set the physics system
+        // Adiciona o sistema de físicas ao jogo
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // Display the bird on the screen
-        this.bird = this.game.add.sprite(100, 245, 'bird');
+        // Exibe o personagem na tela
+        this.personagem = this.game.add.sprite(100, 245, 'personagem');
 
-        // Add gravity to the bird to make it fall
-        game.physics.arcade.enable(this.bird);
-        this.bird.body.gravity.y = 1000;  
+        // adicionar gravidade ao personagem para fazer ele cair
+        game.physics.arcade.enable(this.personagem);
+        this.personagem.body.gravity.y = 1000;  
 
-        // Call the 'jump' function when the spacekey is hit
+        // faz a chamada para a função JUMP a cada vez que a barra de esoaço é pressionada
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);     
 
 
-        this.pipes = game.add.group(); // Create a group  
-        this.pipes.enableBody = true;  // Add physics to the group  
-        this.pipes.createMultiple(20, 'pipe'); // Create 20 pipes  
+        this.blocos = game.add.group(); // Cria um grupo
+        this.blocos.enableBody = true;  // Adiciona
+        this.blocos.createMultiple(20, 'bloco'); // Cria 20 blocos  
 
-        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+        this.timer = game.time.events.loop(1500, this.addRowOfblocos, this); 
 
         this.score = 0;  
         this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
 
         this.jumpSound = game.add.audio('jump');  
 
+        this.personagem.anchor.setTo(-0.2, 0.5);  
+
 
     },
 
     update: function() {
-        // This function is called 60 times per second    
-        // It contains the game's logic   
+        // Esta função é chamada 60 vezes por segundo  
+        // Ela contém a lógica do jogo
 
-        // If the bird is out of the world (too high or too low), call the 'restartGame' function
-        if (this.bird.inWorld == false)
+        // Se o personagem estiver fora do mundo, a função 'restarGame' é chamada
+        if (this.personagem.inWorld == false)
             this.restartGame();
 
-        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);  
+        game.physics.arcade.overlap(this.personagem, this.blocos, this.hitbloco, null, this); 
+
+        if (this.personagem.angle < 20)  
+            this.personagem.angle += 1; 
     },
 
 
-    // Make the bird jump 
+    // faz o  personagem pular
     jump: function() {  
-        // Add a vertical velocity to the bird
-        this.bird.body.velocity.y = -350;
+
+        if (this.personagem.alive == false)  
+            return; 
+
+        // Adiciona velocidade vertical para o personagem
+        this.personagem.body.velocity.y = -350;
 
         this.jumpSound.play();  
+
+
+        // Cria uma animação no personagem
+        var animation = game.add.tween(this.personagem);
+
+        // Configura a animação para alterar o ângulo da imagem do personagem em -20° em 100 milliseconds
+        animation.to({angle: -20}, 100);
+
+        // Inicia a animação
+        animation.start();  
     },
 
-    // Restart the game
+    // Função de reinício de jogo
     restartGame: function() {  
-        // Start the 'main' state, which restarts the game
+        // Inicia o estado principal do jogo, o que reinicia o jogo
         game.state.start('main');
     },
 
 
-    addOnePipe: function(x, y) {  
-        // Get the first dead pipe of our group
-        var pipe = this.pipes.getFirstDead();
+    addOnebloco: function(x, y) {  
+        // Pega o primeiro bloco morto do nosso grupo
+        var bloco = this.blocos.getFirstDead();
 
-        // Set the new position of the pipe
-        pipe.reset(x, y);
+        // Configura a nova posição para o bloco
+        bloco.reset(x, y);
 
-        // Add velocity to the pipe to make it move left
-        pipe.body.velocity.x = -200; 
+        // Adiciona velocidade para o bloco para fazer o movimento para a esquerda
+        bloco.body.velocity.x = -200; 
 
-        // Kill the pipe when it's no longer visible 
-        pipe.checkWorldBounds = true;
-        pipe.outOfBoundsKill = true;
+        // Mata o bloco quando não estiver mais visível
+        bloco.checkWorldBounds = true;
+        bloco.outOfBoundsKill = true;
     },
 
-    addRowOfPipes: function() {  
-        // Pick where the hole will be
+    addRowOfblocos: function() {  
+        // Escolhe onde o buraco será
         var hole = Math.floor(Math.random() * 5) + 1;
 
-        // Add the 6 pipes 
+        // Adiciona os 6 blocos 
         for (var i = 0; i < 8; i++)
             if (i != hole && i != hole + 1) 
-                this.addOnePipe(400, i * 60 + 10);   
+                this.addOnebloco(400, i * 60 + 10);   
 
 
         this.score += 1;  
@@ -110,10 +129,27 @@ var mainState = {
     },
 
 
+    hitbloco: function() {  
+        // Se o personagem tiver batido no bloco, não fazemos nada
+        if (this.personagem.alive == false)
+            return;
+
+        // Configuramos a propriedade de 'vivo' do personagem para falso
+        this.personagem.alive = false;
+
+        // Prevê novos blocos para aparecerem
+        game.time.events.remove(this.timer);
+
+        // Ao passar por todos os blocos, seus movimentos são parados
+        this.blocos.forEachAlive(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
+
 
 
 };
 
-// Add and start the 'main' state to start the game
+// Adiciona e inicia o estado principal para iniciar o jogo
 game.state.add('main', mainState);  
 game.state.start('main');  
